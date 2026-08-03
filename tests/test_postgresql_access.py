af83bcf768e0db5e484f16d4c0cdddadb3dd3563
antononcube <antononcube@posteo.net>
2026-08-03 11:36:29 -0400

    test:Added tests for importing transportation trips.

--- tests/test_postgresql_access.py
+++ tests/test_postgresql_access.py
@@ -67,3 +67,39 @@ def test_import_raw_transportation_trips(database_access):
 
     assert len(dataframe) > 4000
     assert list(dataframe.columns) == expected_columns
+
+
+def test_import_transportation_trips_types_are_parsed(monkeypatch):
+    access = PostgreSQLAccess(DB_CONFIG)
+
+    def _fake_import_dataframe(query, params=None, column_names=None):
+        assert params == ("FAFDerivedLinear",)
+        return pd.DataFrame(
+            [
+                {
+                    "id": "1",
+                    "transportation_trips_id": "FAFDerivedLinear",
+                    "start_lat": "10.5",
+                    "start_lon": "20.5",
+                    "end_lat": "30.5",
+                    "end_lon": "40.5",
+                    "distance": "99.9",
+                    "price": "123.45",
+                    "is_training": "true",
+                }
+            ]
+        )
+
+    monkeypatch.setattr(access, "import_dataframe", _fake_import_dataframe)
+
+    dataframe = access.import_transportation_trips("FAFDerivedLinear")
+
+    assert dataframe.loc[0, "id"] == "1"
+    assert dataframe.loc[0, "transportation_trips_id"] == "FAFDerivedLinear"
+    assert dataframe.loc[0, "start_lat"] == pytest.approx(10.5)
+    assert dataframe.loc[0, "start_lon"] == pytest.approx(20.5)
+    assert dataframe.loc[0, "end_lat"] == pytest.approx(30.5)
+    assert dataframe.loc[0, "end_lon"] == pytest.approx(40.5)
+    assert dataframe.loc[0, "distance"] == pytest.approx(99.9)
+    assert dataframe.loc[0, "price"] == pytest.approx(123.45)
+    assert dataframe.loc[0, "is_training"] is True
